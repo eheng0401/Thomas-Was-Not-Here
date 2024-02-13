90bafb182de39ce78af6285b0912c1403eb7168f
Spambot <spambot@outlook.dk>
2024-02-13 11:26:40 +0100

    asdfas

--- Class Assignment/Assets/Cheats/Scripts/SetVelocityController.cs
+++ Class Assignment/Assets/Cheats/Scripts/SetVelocityController.cs
@@ -30,6 +30,7 @@ public class SetVelocityController : MonoBehaviour
     private void FixedUpdate()
     {
         GroundDetection();
+        Debug.Log("Cookie");
         FlipBehaviour();
         MovementBehaviour();
     }
